c7470c0a67898f7f035e10b8cf270d4b530197ce
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-06-18 16:26:59 +1000

    registered command executor in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin; // essential for functionality
 // import org.bukkit.event.HandlerList
 
 import main.java.listeners.*; // import every listener
+import main.java.command.*; // import every command
 
 public class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
+
+		// Listeners
 		getServer().getPluginManager().registerEvents(new PlayerListener(), this); // register the PlayerListener event
+
+		// Registering Command Executors
+		getCommand("cabbage").setExecutor(new MainCommand(this));
 	}
 	
 	@Override
