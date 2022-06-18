88b7057f071f11a0e01488cfe9630af6dd9f0663
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-06-18 16:39:16 +1000

    added CommandSender to MainCommand

--- src/main/java/command/MainCommand.java
+++ src/main/java/command/MainCommand.java
@@ -3,6 +3,7 @@ package main.java.command;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public class MainCommand implements CommandExecutor {
 
