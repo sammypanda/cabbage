55ded821479e2560f6f0cbe09c2c36379d90f929
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-06-20 11:44:53 +1000

    added code for correctly used /join command

--- src/main/java/command/MainCommand.java
+++ src/main/java/command/MainCommand.java
@@ -34,6 +34,29 @@ public class MainCommand implements CommandExecutor {
             }
         }
 
+        else if (args.length == 2) {
+            
+            if (args[0].equalsIgnoreCase("team") || args[0].equalsIgnoreCase("join")) {
+
+                switch(args[1].toLowerCase()) {
+
+                    case "blue":
+                        sender.sendMessage("You joined the blue team");
+                        break;
+
+                    case "red":
+                        sender.sendMessage("You joined the red team");
+                        break;
+                    
+                    case "green":
+                        sender.sendMessage("You joined the red team");
+                        break;
+                        
+                }
+
+            }
+        }
+
         return false; // if nothing handled say command failed
     }
 }
