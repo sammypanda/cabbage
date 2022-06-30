a517dc16141fff3d31aeb8fad2ee985aeaa57b68
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-06-30 13:02:23 +1000

    material class import: material -> Material

--- src/main/java/command/MainCommand.java
+++ src/main/java/command/MainCommand.java
@@ -14,7 +14,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory;
-import org.bukkit.material;
+import org.bukkit.Material;
 import org.bukkit.attribute.AttributeModifier;
 
 
