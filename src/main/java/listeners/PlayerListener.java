be54c7e27bd2d24e62d5f3dfe87e93aa1fe27f3b
June Mills <gblrfgr@gmail.com>
2022-09-30 12:45:33 -0500

    testing player interactions

--- src/main/java/listeners/PlayerListener.java
+++ src/main/java/listeners/PlayerListener.java
@@ -4,10 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerDrop(PlayerDropItemEvent event) { // when player drops an item
         Bukkit.broadcastMessage("Cabbage saw you drop that!");
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Bukkit.broadcastMessage("Cabbage saw that!");
     }
 }
