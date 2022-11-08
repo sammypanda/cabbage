f11b688093549bcd7b65e50e574f1a623ad28de6
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-11-08 11:57:33 +1000

    get game.ongoing path object as bool instead of object type

--- src/main/java/command/MainCommand.java
+++ src/main/java/command/MainCommand.java
@@ -110,7 +110,7 @@ public class MainCommand implements CommandExecutor {
 
             else if (args[0].equalsIgnoreCase("start")) {
 
-                if (Main.getPlugin().getConfig().get("game.ongoing") == true) {
+                if (Main.getPlugin().getConfig().getBoolean("game.ongoing") == true) {
                     sender.sendMessage("Game already ongoing");
                     return false;
                 }
