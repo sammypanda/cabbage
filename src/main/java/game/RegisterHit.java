345f631b09db3adbfc2aaa44977dd51dd6780ede
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-11-10 11:52:18 +1000

    fixed gamer hit by gamer logic (RegisterHit: Player)

--- src/main/java/game/RegisterHit.java
+++ src/main/java/game/RegisterHit.java
@@ -11,9 +11,10 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.entity.EntityEvent;
 
 public class RegisterHit {
-    private Boolean gamer = false; // is the player a gamer? (currently in a game of cabbage)
-    
     public void Player(EntityDamageByEntityEvent event) {
+        Boolean isGamer = false; // is the player being hit a gamer?
+        Boolean isHitByGamer = false; // is the player doing the hitting a gamer?
+
         // if game not started don't continue
         if (Main.getPlugin().getConfig().getBoolean("game.ongoing") == false) {
             Bukkit.broadcastMessage("game not ongoing, hit doesn't count");
@@ -29,15 +30,23 @@ public class RegisterHit {
         // search for player
         for(String team : Main.getPlugin().getConfig().getConfigurationSection("teams").getKeys(false)) {
             for (String player : Main.getPlugin().getConfig().getConfigurationSection("teams." + team + ".players").getKeys(false)) {
-                if (player.equals(event.getEntity().getUniqueId().toString()) && player.equals(event.getDamager().getUniqueId().toString())) {
-                    this.gamer = true;    
-                
-                    // do hit logic
-                    Bukkit.broadcastMessage("gamer hit gamer");
-                } else {
-                    return;
+                if (player.equals(event.getEntity().getUniqueId().toString())) {
+                    isGamer = true;
+                }
+
+                if (player.equals(event.getDamager().getUniqueId().toString())) {
+                    isHitByGamer = true;
                 }
             }
+        }
+
+        if (isGamer && isHitByGamer) {
+
+            // do hit logic
+            Bukkit.broadcastMessage("gamer hit gamer");
+
+        } else {
+            return;
         }
     }
 }
