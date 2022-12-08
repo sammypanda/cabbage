54d0db3cfe793a4932bbcdc23fd66fc477e2b0ef
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-12-08 21:13:15 +1000

    temporary output test

--- src/main/java/listeners/PlayerListener.java
+++ src/main/java/listeners/PlayerListener.java
@@ -75,7 +75,7 @@ public class PlayerListener implements Listener {
         String color = Team.getPlayerTeam(event.getPlayer().getUniqueId().toString());
         String arena = Main.getPlugin().getConfig().getString("game.arena");
         Location spawn = Main.getPlugin().getConfig().getLocation("arenas." + arena + ".teams." + color + ".spawn");
-        Bukkit.broadcastMessage(color + " moved - " + event.getPlayer().getUniqueId().toString());
+        Bukkit.broadcastMessage(color + " moved: " + event.getTo().toString() + " target: " + spawn.toString());
 
         if (event.getTo().equals(spawn)) {
             Bukkit.broadcastMessage(color + " wins!");
